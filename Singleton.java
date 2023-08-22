4694e97d2b9f596d45435aa9d9e6e1814d9283e7
Muji-47it <107984150+Muji-47it@users.noreply.github.com>
2023-08-22 16:27:19 +0500

    Singleton.java

---
+++ Singleton.java
@@ -0,0 +1,26 @@
+public class Singleton {
+    private static Singleton instance;
+    private Singleton() {
+        
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+ 
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+
+    public void doSomething() {
+        System.out.println("Singleton is doing something.");
+    }
+
+    public static void main(String[] args) {
+
+        Singleton singleton = Singleton.getInstance();
+
+        singleton.doSomething();
+    }
+}
